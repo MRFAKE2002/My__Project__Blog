226a7e2bf288551c4e6c0b2b6cb65933819fe379
R.B <110852196+MRFAKE2002@users.noreply.github.com>
2023-03-15 14:15:45 +0330

    Start app blog and add BlogPost model in it

---
+++ blog/models.py
@@ -0,0 +1,34 @@
+from django.db import models
+from django.utils.translation import gettext_lazy as _
+from django.utils import timezone
+from django.shortcuts import reverse
+
+
+class BlogPost(models.Model):
+    
+    STATUS_CHOICES = [
+        ('dr', 'draft'),
+        ('pu', 'published'),
+    ]
+    
+    title = models.CharField(_('title'), max_length=250)
+    
+    content = models.TextChoices(_('content'))
+    
+    published = models.DateTimeField(_('published'), default = timezone.now())
+    
+    datetime_creation = models.DateTimeField(_('datetime_creation'), auto_now_add = True)
+    
+    datetime_modified = models.DateTimeField(_('datetime_modified'), auto_now = True)
+    
+    is_active = models.BooleanField(_('active'), default = True)
+    
+    status = models.CharField(_('status'), choices=STATUS_CHOICES)
+    
+    def __str__(self):
+        return self.title
+    
+    def get_absolute_url(self):
+        return reverse("post_details", kwargs={"pk": self.pk})
+
+    
--- config/settings.py
+++ config/settings.py
@@ -44,6 +44,7 @@ INSTALLED_APPS = [
     
     # My apps
     'accounts.apps.AccountsConfig',
+    'blog.apps.BlogConfig',
     
 ]
 
@@ -129,3 +130,5 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
