161d7efc02fbc9c8c8c2cfb4461c7aa9648a696f
R.B <110852196+MRFAKE2002@users.noreply.github.com>
2023-03-15 21:21:28 +0330

    Make post list view and add it in url and make its template

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from django.views import generic
 
-# Create your views here.
+from .models import BlogPost
+
+class PostListView(generic.ListView):
+    # model = BlogPost
+    queryset = BlogPost.objects.filter({'status':'pu', 'is_active':'True'}).order_by('published')
+    
+    template_name = 'blog/post_list.html'
+    
+    context_object_name = 'posts'
+
