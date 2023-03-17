aa106f2d791d0f2c51cef3636d07a2efca10c246
R.B <110852196+MRFAKE2002@users.noreply.github.com>
2023-03-17 21:14:10 +0330

    Add front files in static and add its settings

--- config/settings.py
+++ config/settings.py
@@ -142,6 +142,8 @@ LOCALE_PATHS = (
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS = [BASE_DIR, "static"]
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 MEDIA_URL = '/media/'
 
